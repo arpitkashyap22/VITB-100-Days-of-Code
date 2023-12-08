57b4b27e7422ffe9106b0afada701be35345fd68
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2023-12-08 20:28:36 +0530

    done

---
+++ Day28/IsSubset.java
@@ -0,0 +1,22 @@
+package Day28;
+
+public class IsSubset {
+    Arrays.sort(a1);
+    Arrays.sort(a2);
+
+    int i = 0, j = 0;
+
+    while (i < n && j < m) {
+        if (a1[i] < a2[j]) {
+            i++;
+        } else if (a1[i] == a2[j]) {
+            i++;
+            j++;
+        } else {
+            return "No";
+        }
+    }
+
+    // If all elements of a2 are processed, then a2 is a subset of a1
+    return (j == m) ? "Yes" : "No";
+}
