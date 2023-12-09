563513a375bb143e3f81bd031627fafe493a5e1b
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2023-12-09 23:43:29 +0530

    done

---
+++ Day29/ModSum.java
@@ -0,0 +1,22 @@
+public class ModSum {
+    int Maximize(int arr[], int n)
+    {
+        // Complete the function
+        
+       
+        int mod = 1000000007;
+     
+        // Sorting the array in ascending order
+        Arrays.sort(arr);
+        
+        long ans = 0;
+        
+        // Calculating the sum of product of each element with its index
+        // and taking modulo to avoid overflow
+        for(int i=0;i<n;i++)
+            ans = (ans + ((long)arr[i]*i)%mod )%mod;
+        
+        return (int)ans;
+
+    }   
+}
