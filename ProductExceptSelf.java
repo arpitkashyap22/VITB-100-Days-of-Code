e75ec9d239c8ef14285adfe526bf512d0eb868bb
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2023-12-13 21:03:52 +0530

    done

---
+++ ProductExceptSelf.java
@@ -0,0 +1,29 @@
+public class ProductExceptSelf {
+    public static long[] productExceptSelf(int nums[], int n) 
+	{ 
+        // code here
+        long ans[] = new long[n];
+        
+         for(int i = 0; i<n; i++){
+            ans[i] = 1;
+        }
+        
+        int i = 0;
+        int j = 1;
+        
+        while(i<n && j<n){
+            if(j == i){
+                j++;
+                continue;
+            }
+            ans[i] = ans[i]*nums[j];
+            j++;
+            if(j==n){
+                i++;
+                j =0;
+            }
+        }
+        
+        return ans;
+	} 
+}
