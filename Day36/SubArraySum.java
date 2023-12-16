a0648c71280b8ba81d3901493a02d065d75e9230
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2023-12-16 15:07:14 +0530

    done

---
+++ Day36/SubArraySum.java
@@ -0,0 +1,31 @@
+public class SubArraySum {
+    public static int smallestSubWithSum(int a[], int n, int x) {
+        // Your code goes here 
+         int min_length = n + 1;
+
+        int i = 0;
+        int j = 0;
+        int currSum = 0;
+
+        while (j < n) {
+            while (currSum <= x && j < n) {
+                if (currSum <= 0 && x > 0) {
+                    i = j;
+                    currSum = 0;
+                }
+                currSum = currSum + a[j];
+                j++;
+            }
+
+            while (currSum > x && i < n) {
+                if (j - i < min_length) {
+                    min_length = j - i;
+                }
+                currSum = currSum - a[i];
+                i++;
+            }
+        }
+
+        return (min_length == n + 1) ? 0 : min_length;
+    }
+}
