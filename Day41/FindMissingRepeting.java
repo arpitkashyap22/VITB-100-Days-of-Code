059941e905f9677a65e30e09d88a313d1514497f
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2023-12-21 18:06:54 +0530

    done

---
+++ Day41/FindMissingRepeting.java
@@ -0,0 +1,49 @@
+package Day41;
+
+public class FindMissingRepeting {
+    int[] findTwoElement(int arr[], int n) {
+        int[] result = new int[2];
+       
+       // Find repeating number (B)
+       for (int i = 0; i < n; i++) {
+           int index = Math.abs(arr[i]) - 1;
+           if (arr[index] < 0) {
+               result[0] = Math.abs(arr[i]);
+           } else {
+               arr[index] = -arr[index];
+           }
+       }
+       
+       // Find missing number (A)
+       for (int i = 0; i < n; i++) {
+           if (arr[i] > 0) {
+               result[1] = i + 1;
+               break;
+           }
+       }
+       
+       return result;
+   } int[] findTwoElement(int arr[], int n) {
+    int[] result = new int[2];
+   
+   // Find repeating number (B)
+   for (int i = 0; i < n; i++) {
+       int index = Math.abs(arr[i]) - 1;
+       if (arr[index] < 0) {
+           result[0] = Math.abs(arr[i]);
+       } else {
+           arr[index] = -arr[index];
+       }
+   }
+   
+   // Find missing number (A)
+   for (int i = 0; i < n; i++) {
+       if (arr[i] > 0) {
+           result[1] = i + 1;
+           break;
+       }
+   }
+   
+   return result;
+}
+}
