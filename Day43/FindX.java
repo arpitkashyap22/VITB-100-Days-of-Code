bea31e8285fe63f2d774a5eda8f321be681f1534
arpitkashyap22 <64071429+arpitkashyap22@users.noreply.github.com>
2023-12-22 17:10:29 +0530

    done

---
+++ Day43/FindX.java
@@ -0,0 +1,17 @@
+package Day43;
+
+public class FindX {
+    public static int search (int arr[], int n, int x, int k) {
+        //Complete the function
+          int i = 0;
+        while (i < n) {
+            if (arr[i] == x) {
+                return i;
+            }
+
+            i = i + Math.max(1, Math.abs(arr[i] - x) / k);
+        }
+
+        return -1;
+    }
+}
